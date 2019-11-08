a3d69e18f1f7c405816337cb1c32efb5818959a8
elisha <elisha@elishadeMBP.lan>
2019-11-09 01:05:59 +0800

    update vimrc_install.sh

--- vimrc_install.sh
+++ vimrc_install.sh
@@ -7,7 +7,7 @@ cd ~/.vim
 echo 'set runtimepath+=~/.vim
 source ~/.vim/vimrc.d/basic.vim
 try
-	source ~/.vim/vimrc.d/function.vim
+    source ~/.vim/vimrc.d/function.vim
     source ~/.vim/vimrc.d/plugins.vim
     source ~/.vim/vimrc.d/nerdtree_config.vim    
 catch
